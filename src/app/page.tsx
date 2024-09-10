500615c6aca35ebaf5091b2febc60786195081d0
JackEdward Oliver <jack@jv-la.com>
2024-09-10 11:37:57 +0200

    All the things

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,18 @@
 export default function Home() {
   return (
-    <>
-      <header>Jack-Edward Oliver</header>
-      <main>
-        <p>
-          Staff UX Engineer @ <a href="https://cloudbees.com">CloudBees</a>.
-          Find me on <a href="https://linkedin.com/in/mrjackoliver">LinkedIn</a>{" "}
-          or <a href="https://github.com/jackoliver">GitHub</a>.
-        </p>
-      </main>
-    </>
+    <main>
+      <p>
+        Jack-Edward Oliver. <br />
+        <br />
+        Currently in Göteborg, Sweden. Originally from Oxford, UK.
+        <br />
+        Working as a Staff UX Engineer @{" "}
+        <a href="https://cloudbees.com">CloudBees</a>. <br />
+        <br /> Find me on{" "}
+        <a href="https://linkedin.com/in/mrjackoliver">LinkedIn</a> /{" "}
+        <a href="https://github.com/jackoliver">GitHub</a>. Or{" "}
+        <a href="mailto:jack@jv-la.com">send me an email</a>.
+      </p>
+    </main>
   );
 }
