40ee089126b97758034c06b0ba2c6fde995f1059
JackEdward Oliver <jack@jv-la.com>
2024-09-10 14:44:10 +0200

    Stuff

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,4 +1,44 @@
-export default function Home() {
+import { getAllBooks } from "@/lib/api";
+import Image from "next/image";
+
+const BookItem = ({ id, title, rating, genres, cover, link, summary }) => (
+  <li key={id}>
+    {/* <Image
+      width={200}
+      height={200}
+      className="object-fit"
+      src={cover.url}
+      alt={title}
+    /> */}
+    <a target="_blank" href={link}>
+      {title}
+    </a>{" "}
+    {rating}/5 {genres.map((genre: string) => genre).join(", ")}
+    <br />
+    <br />
+    <em>{summary}</em>
+    <br />
+    <br />
+  </li>
+);
+
+const Library = ({ books }) => (
+  <>
+    <br />
+    <h2>Library</h2>
+    <hr />
+    <br />
+    <ul>
+      {books.map((book) => (
+        <BookItem key={book.id} {...book} />
+      ))}
+    </ul>
+  </>
+);
+
+export default async function Home() {
+  const books = await getAllBooks();
+
   return (
     <main>
       <p>
@@ -13,6 +53,7 @@ export default function Home() {
         <a href="https://github.com/jackoliver">GitHub</a>. Or{" "}
         <a href="mailto:jack@jv-la.com">send me an email</a>.
       </p>
+      {/* <Library books={books} /> */}
     </main>
   );
 }
